d421bfba40b97b56e551eb7e13534c9a89a05bb0
Lyra1337 <lyra@lyralabs.net>
2015-11-15 02:00:05 +0100

    initial commit

---
+++ kkmate-web/kkmate-web/js/main.js
@@ -0,0 +1,20 @@
+﻿(function() {
+    var Program = new function() {
+        this.init = function() {
+            this.bindEvents();
+        };
+
+        this.run = function() {
+            this.init();
+        };
+
+        this.bindEvents = function() {
+            var bla = document.querySelector('');
+        };
+    };
+
+    var program = new Program();
+
+    program.run();
+
+}());
